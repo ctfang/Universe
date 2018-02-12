f263e6c60f44b884482c016597723f791035c472
baichou <baichou@qingmutec.com>
2018-02-12 11:25:46 +0800

    集成blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,15 +68,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    视图模板
                 </div>
             </div>
         </div>
